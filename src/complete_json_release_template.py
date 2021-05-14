1a024d2e620c30b4cd61bb7fb367c55ff82d9c79
Ruth Petrie <rpetrie@sci5.jasmin.ac.uk>
2021-05-14 14:35:45 +0100

    comment json template code

--- src/complete_json_release_template.py
+++ src/complete_json_release_template.py
@@ -124,25 +124,6 @@ def main():
 
 
 
-
-        # for index, row in cfres_dfs_df.iterrows():
-        #     print(index, row)
-        #     asdf
-        #     qc_template["datasets"][id]['files'] =
-        #
-        # json_files[row.pid] = {'filename': os.path.basename(row.filepath),
-        #                                   'qc_status': ['fail' if not row.error_level else 'pass'][0],
-        #                                   'error_severity': row.cf_severity_level,
-        #                                   'error_message': [row.error_details if row.error_details else 'na'][0]
-        #                                   }
-        #             if not row.error_level == 'pass':
-        #                 ds_qcStatus = "fail"
-        #
-        #
-        # qc_template["datasets"][id]["qc_status"] = 'tbc'
-        # # files
-        # for file in
-
 def get_dataset_pids(filename):
     logging.debug(f'Reading release datasets {filename}')
     d = {}
@@ -150,62 +131,6 @@ def get_dataset_pids(filename):
         for line in reader:
             d[line.split(',')[1].strip()] = line.split(',')[0].strip()
     return d
-
-
-# def main(datasets_file, dataframe_file, missing_file, ofile):
-#
-#     # Read in data
-#     datasets_dict = get_dataset_pids(datasets_file)
-#     logging.info(f'Reading qc dataframe {dataframe_file}')
-#     dfo = pd.read_pickle(dataframe_file)
-#     df = dfo[dfo['filepath'].notna()]
-#     # print(df.iloc[3])
-#
-#
-#     # Set up json header
-#     jout = {}
-#     jout["header"] = {"application:": 'CF-checker', 'Author': 'Ruth Petrie', 'Institution': 'CEDA', 'Date': dt.today().isoformat(), 'version': '1.0'}
-#     jout["results"] = {}
-#     jout["datasets"] = {}
-#     # Loop over all datasets
-#     for dsid, pid in list(datasets_dict.items()):
-#
-#         ddir = os.path.join('/badc/cmip6/data/', dsid.replace('.', '/'))
-#         if not os.path.exists(ddir):
-#             with open(f'{missing_file}', 'a+') as w:
-#                 w.writelines(f'{dsid}\n')
-#             continue
-#         if len(os.listdir(ddir)) == 0:
-#             with open(f'{missing_file}', 'a+') as w:
-#                 w.writelines(f'{dsid}\n')
-#             continue
-#
-#         dataset_records = df[df.filepath.str.contains(dsid.replace('.', '/'))].reset_index(drop=True)
-#         if not len(dataset_records) > 0:
-#             with open(f'{missing_file}', 'a+') as w:
-#                 w.writelines(f'QC_MISSING: {dsid}\n')
-#             continue
-#
-#         json_ds_qc = {'error_severity': 'na', 'error_message': 'na'}
-#         ds_qcStatus = "pass"
-#
-#         json_files = {}
-#
-#         for index, row in dataset_records.iterrows():
-#             json_files[row.pid] = {'filename': os.path.basename(row.filepath),
-#                                   'qc_status': ['fail' if not row.error_level else 'pass'][0],
-#                                   'error_severity': row.cf_severity_level,
-#                                   'error_message': [row.error_details if row.error_details else 'na'][0]
-#                                   }
-#             if not row.error_level == 'pass':
-#                 ds_qcStatus = "fail"
-#
-#         jout["datasets"][pid] = {'dset_id': dsid, 'qc_status': ds_qcStatus, 'dataset_qc': json_ds_qc, 'files': json_files}
-#
-#     # Output JSON
-#     json_obj = json.dumps(jout, indent=4)
-#     with open(f"{ofile}", "a+") as o:
-#         o.write(json_obj)
 
 
 if __name__ == "__main__":
